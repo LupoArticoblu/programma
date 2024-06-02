00f1a8c66a09c73c37f6cefaac17a290ee5fcb75
LupoArticoblu <wolf94-@libero.it>
2024-06-02 10:46:00 +0200

    inizio griglia

--- 06 functions/Arrow function/script.js
+++ 06 functions/Arrow function/script.js
@@ -5,3 +5,17 @@ let isGiorno = false;
 const saluto = (isGiorno === true) ? "Buongiorno" : "Buonasera";
 
 console.log(saluto);
+
+//Scope: la funzione dello scope la conosciamo già, meno globale = meno possibilità di conflitto = meno errori e più riuso, una cosa che è doverosa ricordare è che dichiarando una variabile (const, let o anche var) non sarà mai visibile all'esterno dello scope, mentre dichiarare una variabile senza suffisso...
+
+function fuoriScope(){
+  const no = 'non lo vedo';
+  let neanche = 'non lo vedo';
+  var quandoMai = 'non lo vedo';
+  si = 'lo vedo';
+}
+
+  console.log(no);
+  console.log(neanche);
+  console.log(quandoMai);
+  console.log(si); //forse non è più possibile vedere un qualcosa di non dichiarato come variabile al di fuori dello scope
