2ef8f3c0dbeb0216e03767694deb5d9301f17b98
LupoArticoblu <wolf94-@libero.it>
2024-06-12 17:09:11 +0200

    spiegazione dati object

---
+++ 07 object/script.js
@@ -0,0 +1,37 @@
+//gli oggetti si distinguono dagli array perchè sono contenitori di proprietà e non di sole stringhe o numeri
+
+//array
+const array = ['beppe','tomi','via sanchez II', 21];
+
+//object
+const oggetto ={
+  //chiave:valore(qualsiasi tipo di dato)
+  nome:'beppe', //<- virgola tra una proprietà e l'altra
+  'cognome':'tomi', // <- chiave con apici
+  via: 'via sanchez II', // <- chiave senza apici
+  anni: 21,
+  hobbies: ['pattinaggio','alpinismo','leggere']
+};
+
+//da leggere con la dotNotation perchè sono proprietà...
+console.log(oggetto.nome);
+
+//...o con le parentesi quadre come fossero figli di un array
+console.log(oggetto['cognome']);
+
+//posso anche usare una variabile come chiave attribuendole il valore della chiave stessa usata nell'oggetto
+let key = 'via';
+//la vera utilità della square braket notation per gli oggetti
+console.log(oggetto[key]);
+console.log(oggetto.key);//<- la dotNotation qui restituisce undefined
+
+console.log("prima della riassegnazione",oggetto.anni);
+//le proprietà possono essere riassegnate(lettura/scrittura)
+oggetto.anni = 31;
+
+console.log("dopo la riassegnazione",oggetto.anni);
+
+//aggiungo una proprietà
+oggetto.telefono = 3270000001;
+
+console.log(oggetto);
