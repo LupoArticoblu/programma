912082e848775c8024918185a3e6c5bce7264f60
LupoArticoblu <wolf94-@libero.it>
2024-06-16 13:26:12 +0200

    snack (automobili, nomi)

--- 04 cicli/ciclo for each/script.js
+++ 04 cicli/ciclo for each/script.js
@@ -68,3 +68,86 @@ const nomiCognomi = studentiMaggiorenni.map(studente => {
   return studente.nome + ' ' +studente.cognome
 });
 console.log(nomiCognomi);
+
+//array di oggetti con 10 automobili: marca, modello e alimentazione(benzina, diesel, gpl, elettrica)
+
+const automobili = [
+  {
+    marca: "Audi",
+    modello: "Q7",
+    alimentazione: "diesel"
+  },
+  {
+    marca: "BMW",
+    modello: "X5",
+    alimentazione: "diesel"
+  },
+  {
+    marca: "Fiat",
+    modello: "Panda",
+    alimentazione: "benzina"
+  },
+  {
+    marca: "Ford",
+    modello: "Mustang",
+    alimentazione: "diesel"
+  },
+  {
+    marca: "Mercedes",
+    modello: "C63",
+    alimentazione: "elettrica"
+  },
+  {
+    marca: "Nissan",
+    modello: "GTR",
+    alimentazione: "gpl"
+  },
+  {
+    marca: "Renault",
+    modello: "Megane",  
+    alimentazione: "diesel"
+  },
+  {
+    marca: "Volkswagen",
+    modello: "Golf",
+    alimentazione: "benzina"
+  },
+  {
+    marca: "Toyota",
+    modello: "Yaris",
+    alimentazione: "diesel"
+  },  
+  {
+    marca: "Volkswagen",
+    modello: "Passat",
+    alimentazione: "diesel"
+  }
+];  
+//dividere in 3 array: auto a benzina, auto diesel, altre auto
+const autoBenzina = automobili.filter(auto => auto.alimentazione === 'benzina');
+
+const autoDiesel = automobili.filter(auto => auto.alimentazione === 'diesel');
+
+const altreAuto = automobili.filter(auto => auto.alimentazione !== 'benzina' && auto.alimentazione !== 'diesel');
+
+//stampo separatamente i 3 array
+console.log(autoBenzina);
+console.log(autoDiesel);
+console.log(altreAuto);
+
+
+
+
+//creare un array di stringhe con nomi e 'case' diversi
+const nomiCaotici = [
+  'luca',
+  'maria',
+  'GIOVANNI',
+  'FaUSto' 
+];
+console.log(nomiCaotici);
+//dal primo creiamo un nuovo array formattato con tutte le prime lettere maiuscole
+const nomiFormattati = nomiCaotici.map(nome => {
+  return nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
+});
+console.log(nomiFormattati);
